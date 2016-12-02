e0fff67cc26df6749bad49d63d9fc1338c825466
laurenpalermo10 <lpalermo@sbacademy.org>
2016-12-02 15:46:50 -0800

    Add files via upload

---
+++ flowMatic.rb
@@ -0,0 +1,22 @@
+=begin
+|~~ |~~
+| |
+:$: HIS MAJESTY'S FLOWMATIC SOMETHING-OR-OTHER :$:
+`'''''''''''''''''''''''''''''''''''''''''''''`
+~= Instructions =~
+1. Water should flow if flowmatic_on is true and
+water_available is true.
+2. If flowmatic_on is false, the message
+"Flowmatic is off!" should appear.
+3. If water_available is false, the message
+"No water!" should appear.
+4. If the flow_rate is above 50, the warning
+"Warning! flow_rate is above 50!" should
+appear, along with the current flow rate.
+5. If the flow_rate is below 50, the warning
+"Warning! flow_rate is below 50!" should
+appear, along with the current flow rate.
+=end
+
+flowmatic_on=
+water_available=
